b0022700d7e5162b8487d3ed238369231ef6002f
Alexander Hedling <taikes_bass@hotmail.com>
2022-04-26 21:32:54 +0200

    första basversion inför snabbvisning

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,15 @@
-console.log("Har laddats OK")
+document.querySelector("nav button").addEventListener("click", flipAriaExpanded)
+document.querySelectorAll(".systems li[aria-expanded]").forEach(element => {
+    element.addEventListener("click", flipAriaExpanded)
+})
+
+
+function flipAriaExpanded() {
+    const ariaElement = this.closest("[aria-expanded]")
+    const currentState = ariaElement.ariaExpanded
+
+    let newState = "false"
+    if (!currentState || currentState === "false") newState = "true"
+
+    ariaElement.ariaExpanded = newState
+}
